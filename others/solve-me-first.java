ecfa5210d8d185408c9ad12e47ce27860a6e3166
eduard0803 <eduardo83204@gmail.com>
2023-12-19 20:40:32 -0300

    add exercise

---
+++ others/solve-me-first.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+// link: https://vjudge.net/problem/HackerRank-solve-me-first/origin
+public class Main{
+    private static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        int a = sc.nextInt(),
+            b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
